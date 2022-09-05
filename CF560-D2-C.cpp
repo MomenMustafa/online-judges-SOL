a056666c3ebd9bd1899036c25c2d25b12725888a
Mo'men <zaaza1233@gmail.com>
2022-09-05 22:12:25 +0200

    CF560-D2-C Gerald's Hexagon

---
+++ CF560-D2-C.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    int a1,a2,a3,a4,a5,a6;
+    scanf("%d %d %d %d %d %d", &a1, &a2, &a3, &a4, &a5, &a6);
+    printf("%d", (a1+a3+a2)*(a1+a3+a2)-(a1*a1+a3*a3+a5*a5));
+    return 0;
+}
